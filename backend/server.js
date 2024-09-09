54828522193a5be9722b63a46875672eab6059bb
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2024-09-09 11:48:49 +0530

    Backend Completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
+//Package Import
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser";
+
+//Routes conncetions
+import messageRoutes from "./routes/message.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+//Database Connection
 import connectToMongoDb from "./DB/connectToMongoDb.js";
 
 const app = express();
@@ -13,10 +21,13 @@ dotenv.config();
 // });
 
 app.use(express.json()); // to parse the incoming requests with json payloads (from req.body)
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    connectToMongoDb();
-    console.log(`Server is running on port ${PORT}`)
+  connectToMongoDb();
+  console.log(`Server is running on port ${PORT}`);
 });
