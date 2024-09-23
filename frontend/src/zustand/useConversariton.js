8ff86565127e8f50d0688462e33c7fe6c6ae9b0d
Himanshu Kumar <himanshu.2201056cs@iiitbh.ac.in>
2024-09-24 00:38:17 +0530

    zustand work started

--- frontend/src/zustand/useConversariton.js
+++ frontend/src/zustand/useConversariton.js
@@ -1,10 +1,11 @@
 import {create} from 'zustand';
 
-const useConversariton = create(set) =>{
-    [
-        
-    ]
-}
+const useConversariton = create(set) => ({
+    selectedConversation : null,
+    setSelectedConversation : (selectedConversation) => set({selectedConversation}),
+    messages : [],
+    setMessages : (messages) => set({messages}),
+})
 
 export default useConversariton
 
